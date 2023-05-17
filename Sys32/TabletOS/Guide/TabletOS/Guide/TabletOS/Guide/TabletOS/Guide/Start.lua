84af29569eb7068f6117fcef28512ce2ce0acddb
Matveymayner <127684055+Matveymayner@users.noreply.github.com>
2023-05-17 19:27:33 +0300

    Start.lua

---
+++ Sys32/TabletOS/Guide/TabletOS/Guide/TabletOS/Guide/TabletOS/Guide/Start.lua
@@ -0,0 +1,20 @@
+return {
+	["eu_RU"] = {
+		name = "Добро пожаловать в графическую оболочку MaynerOS Donate",
+		icon = "W",
+		description = [[Чувствуйте себя, как дома. Нажмите на уведомление, что бы развернуть его. 
+Что бы провести взаимодействие с файлом/папкой или текущей директорией, нажмите правую кнопку мыши на файле, папке или пустом пространстве.
+Что бы убрать уведомление - нажмите на крестик в его уголке.
+Вы получите дополнительные инструкции по мере появления ситуаций, где их можно использовать.
+		},
+	},
+	["eu_EN"] = {
+		name = "Welcome to MaynerOS Donate GUI",
+		icon = "W",
+		description = [[Make yourself at home. Click on the notification to expand it.
+		To interact with a file/folder or current directory, right-click a file, folder or empty space.
+		To remove the notification - click on the cross in its corner.
+		You will receive additional instructions on the occurrence of situations where they can be used.
+		},
+	}
+}
